51d602d961d302ff511a11068cfefd1ba16f6750
keni <kenikakadiya@gmail.com>
2024-04-10 09:23:17 +0530

    4-10-24

---
+++ Lecture-3/myapp/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Para1,{Para2,Para3,Para4,Para5} from './Compnent/Para';
+
+function App() {
+  return (
+    <div className="App">
+      <Para1/>
+      <Para2/>
+      <Para3/>
+      <Para4/>
+      <Para5/>
+    </div>
+  );
+}
+
+export default App;
